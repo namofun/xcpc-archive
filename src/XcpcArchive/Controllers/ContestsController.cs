8917268f220bdb22ebcd81b3ab734f0b4122c9d4
XiaoYang <tlylz99@live.cn>
2021-12-31 01:09:29 +0800

    Update layout

--- src/XcpcArchive/Controllers/ContestsController.cs
+++ src/XcpcArchive/Controllers/ContestsController.cs
@@ -6,6 +6,7 @@ namespace XcpcArchive.Controllers
     public class ContestsController : Controller
     {
         [HttpGet]
+        [HttpGet("/")]
         public IActionResult Index()
         {
             return View();
